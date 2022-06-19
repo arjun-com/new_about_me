d8c0a2b3c2c0626bbfd36eb67790d4bc7b0c9533
sssss <85511086+ClxppyLOL@users.noreply.github.com>
2022-06-19 10:56:34 +0530

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,6 @@ function sleep(ms) {
 }
 
 async function char_loadin() {
-    document.getElementById("devices").style.opacity = 100
     const elements = document.getElementsByClassName("welc_letter")
     for(var x = 0; x < elements.length; x++) {
         elements[x].style.borderLeft = "2px solid pink"
@@ -14,6 +13,15 @@ async function char_loadin() {
     }
     await sleep(750)
     document.getElementById("welc_desc").style.opacity = 100
+}
+
+async function loadin() {
+    await sleep (5500)
+    document.getElementById("devices").style.opacity = 100
+    for(var i = 0; i < (document.getElementsByClassName("about-div")).length; i++) {
+        document.getElementsByClassName("about-div")[i].style.opacity = 100
+    }
+    document.getElementById("msg").style.opacity = 100
 }
 
 async function tmp_underline() {
@@ -37,7 +45,22 @@ async function tmp_underline() {
     }
 }
 
+async function mouse_trail() {
+    const trail = document.createElement("p")
+    trail.setAttribute("id", "trail")
+    document.body.appendChild(trail)
+    document.addEventListener("mousemove", (details) => {
+        console.log(details)
+        trail.style.top = ( details.clientY + 5 ) + "px"
+        trail.style.left = ( details.clientX + 5 )+ "px"
+    })
+}
+
 char_loadin()
+
+mouse_trail()
+
+loadin()
 
 tmp_underline()
 
